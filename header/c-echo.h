dad75bf6ab1325bc00e387588d5b168c8917bd43
Nate862207104 <nbern014@hammer.cs.ucr.edu>
2021-07-30 18:32:36 -0700

    add one more rectangle and compute its area

---
+++ header/c-echo.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+std::string echo(int length, char** chars) {
+	std::string ret = "";
+	for (int i = 1; i<length; i++)
+		ret += char[i];
+		if (i<length){
+	 		ret +=" ";
+		}
+	}
+	ret += "\n";
+	return ret;
+}
+
+	
---
+++ src/main2.cpp
@@ -0,0 +1,6 @@
+#include "/lab-01/header/c-echo.h"
+
+int main(int argv, char arg c) {
+	std::cout << echo(argv,argc);
+}
+
